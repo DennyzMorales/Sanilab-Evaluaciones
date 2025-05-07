d40f6f83908e5336e42c9ef6c9b6c368a253e65b
Denny <d.morales.fcalma@gmail.com>
2025-05-06 20:52:41 -0500

    Cors activated locally

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const autoevaluacionesRoutes = require('./routes/autoevaluaciones');
 const empleadosRoutes = require('./routes/empleados');
 const administradoresRoutes = require('./routes/administradores');
 const authRoutes = require('./routes/auth');
-app.use(cors()); // Habilita CORS
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true // si vas a usar cookies o headers de autenticación
+}));
 // Usamos express.json() para manejar el cuerpo de las solicitudes con formato JSON
 app.use(express.json());
 app.use('/api/autoevaluaciones', autoevaluacionesRoutes);
 app.use('/api/empleados', empleadosRoutes);
 app.use('/api/administradores', administradoresRoutes);
 app.use('/api/auth',authRoutes)
+
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:3000');
+  console.log('Servidor corriendo en el puerto 3000');
 });
