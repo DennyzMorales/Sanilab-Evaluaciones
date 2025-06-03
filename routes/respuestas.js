7b2ff316817e2daeefd184b3fbc2940db19410d9
Denny <d.morales.fcalma@gmail.com>
2025-06-03 02:20:41 -0500

    router fix

--- routes/respuestas.js
+++ routes/respuestas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require("../controllers/autoevaluacionRespuestasController");
 
-router.post('/', ctrl.crearRespuestaAutoevaluacion);
+router.post('/', ctrl.crearRespuestasAutoevaluacion);
 router.post('/docs', ctrl.enviarUltimaRespuestaAGoogleDocs);
 
 module.exports = router;
